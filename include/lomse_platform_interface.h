8321ccfbf65804b36252f82c92ae6d8a29f8d4fd
cecilios <>
2012-02-04 18:07:56 +0000

    License changed to BSD 2-clause license

--- include/lomse_platform_interface.h
+++ include/lomse_platform_interface.h
@@ -1,23 +1,32 @@
-//----------------------------------------------------------------------------
-// Anti-Grain Geometry - Version 2.4
-// Copyright (C) 2002-2005 Maxim Shemanarev (http://www.antigrain.com)
+//---------------------------------------------------------------------------------------
+// This file is part of the Lomse library.
+// Copyright (c) 2010-2012 Cecilio Salmeron. All rights reserved.
 //
-// Permission to copy, use, modify, sell and distribute this software
-// is granted provided this copyright notice appears in all copies.
-// This software is provided "as is" without express or implied
-// warranty, and with no claim as to its suitability for any purpose.
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
 //
-//----------------------------------------------------------------------------
-// Contact: mcseem@antigrain.com
-//          mcseemagg@yahoo.com
-//          http://www.antigrain.com
-//----------------------------------------------------------------------------
+//    * Redistributions of source code must retain the above copyright notice, this 
+//      list of conditions and the following disclaimer.
 //
-// class platform_support
+//    * Redistributions in binary form must reproduce the above copyright notice, this
+//      list of conditions and the following disclaimer in the documentation and/or
+//      other materials provided with the distribution.
 //
-// It's not a part of the AGG library, it's just a helper class to create
-// interactive demo examples. Since the examples should not be too complex
-// this class is provided to support some very basic interactive graphical
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+// SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+//
+// For any comment, suggestion or feature request, please contact the manager of
+// the project at cecilios@users.sourceforge.net
+//---------------------------------------------------------------------------------------
+
 // funtionality, such as putting the rendered image to the window, simple
 // keyboard and mouse input, window resizing, setting the window title,
 // and catching the "idle" events.
--- include/lomse_score_player_ctrl.h
+++ include/lomse_score_player_ctrl.h
@@ -1,21 +1,30 @@
 //---------------------------------------------------------------------------------------
-//  This file is part of the Lomse library.
-//  Copyright (c) 2010-2011 Lomse project
+// This file is part of the Lomse library.
+// Copyright (c) 2010-2012 Cecilio Salmeron. All rights reserved.
 //
-//  Lomse is free software; you can redistribute it and/or modify it under the
-//  terms of the GNU General Public License as published by the Free Software Foundation,
-//  either version 3 of the License, or (at your option) any later version.
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
 //
-//  Lomse is distributed in the hope that it will be useful, but WITHOUT ANY
-//  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-//  PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//    * Redistributions of source code must retain the above copyright notice, this 
+//      list of conditions and the following disclaimer.
 //
-//  You should have received a copy of the GNU General Public License along
-//  with Lomse; if not, see <http://www.gnu.org/licenses/>.
+//    * Redistributions in binary form must reproduce the above copyright notice, this
+//      list of conditions and the following disclaimer in the documentation and/or
+//      other materials provided with the distribution.
 //
-//  For any comment, suggestion or feature request, please contact the manager of
-//  the project at cecilios@users.sourceforge.net
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+// SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
 //
+// For any comment, suggestion or feature request, please contact the manager of
+// the project at cecilios@users.sourceforge.net
 //---------------------------------------------------------------------------------------
 
 #ifndef _LOMSE_SCORE_PLAYER_CTRL_H__
