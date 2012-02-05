00afa7ea624876186b85e10548c37a4ebd67025f
cecilios <>
2012-02-05 13:15:53 +0000

    fix compilation warnings

--- include/lomse_drawer.h
+++ include/lomse_drawer.h
@@ -5,14 +5,14 @@
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
 //
-//    * Redistributions of source code must retain the above copyright notice, this 
+//    * Redistributions of source code must retain the above copyright notice, this
 //      list of conditions and the following disclaimer.
 //
 //    * Redistributions in binary form must reproduce the above copyright notice, this
 //      list of conditions and the following disclaimer in the documentation and/or
 //      other materials provided with the distribution.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
 // OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
 // SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
@@ -97,13 +97,13 @@ struct RenderOptions
     bool cast_shadow_flag;
 
     RenderOptions()
-        : scale(1.0)
+        : draw_anchors(false)
+        , scale(1.0)
         , background_color(127, 127, 127)   //grey
         , highlighted_color(255,0,0)        //red
         , selected_color(0,0,255)           //blue
         , page_border_flag(true)
         , cast_shadow_flag(true)
-        , draw_anchors(false)
     {
         boxes.reset();
     }
