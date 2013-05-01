aa385ab3d7fe1adb5298e399b6ed83682fdfbf46
Cecilio Salmeron <s.cecilios@gmail.com>
2013-05-01 16:45:42 +0200

    Add missing file liblomse.pc.cmake

---
+++ liblomse.pc.cmake
@@ -0,0 +1,14 @@
+# pkg-config source file
+
+libdir=@LOMSE_LIBDIR@
+includedir=@LOMSE_INCLUDEDIR@
+
+Name: liblomse
+Description: LenMus open music score edition library
+Version: @LOMSE_VERSION_STRING@
+Requires: @LOMSE_REQUIRES@
+Libs: -L${libdir} -llomse
+Cflags: -I${includedir}
+
+
+
