d5a5079195cf58548f3e575ffe81bd9af2c47ef9
cecilios <s.cecilio@gmail.com>
2016-03-03 15:21:44 +0100

    Update version information for new release

--- include/lomse_version.h
+++ include/lomse_version.h
@@ -2,5 +2,5 @@
 // This file is part of the Lomse library.
 #define LOMSE_VERSION_MAJOR    0
 #define LOMSE_VERSION_MINOR    17
-#define LOMSE_VERSION_PATCH    15
+#define LOMSE_VERSION_PATCH    19
 
