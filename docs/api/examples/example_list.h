e06baa0d8873c32b6a4bf4785f44b2569968904e
cecilios <s.cecilio@gmail.com>
2018-03-03 17:09:27 +0100

    Add all examples to API documentation

---
+++ docs/api/examples/example_list.h
@@ -0,0 +1,37 @@
+/**
+@example example_1_wxwidgets.cpp
+Code for wxWidgets (platform independent). Example 1: How to display a music score.
+
+@example example_2_wxwidgets.cpp
+Code for wxWidgets (platform independent). Example 2: Interacting with the score.
+
+@example example_3_wxwidgets.cpp
+Code for wxWidgets (platform independent). Example 3: Playing back scores.
+
+@example example_4_wxwidgets.cpp
+Code for wxWidgets (platform independent). Example 4: Visual tracking during playback.
+
+
+
+@example example_1_qt.h
+Header for Qt (platform independent). Example-1: How to display a music score
+
+@example example_1_qt.cpp
+Code for Qt (platform independent). Example-1: How to display a music score
+
+
+
+@example example_1_x11.cpp
+Code for Linux with X11. Example 1: How to display a music score.
+
+@example example_2_x11.cpp
+Code for Linux with X11. Example 2: Interacting with the score.
+
+
+
+@example example_1_win32.cpp
+Code for WindowsXP. Example 1: How to display a music score.
+
+
+*/
+
