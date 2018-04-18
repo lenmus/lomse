965d3ea3d1711856652b05a3c5bf98d1e647a044
Andrey Prygunkov <hugbug@users.sourceforge.net>
2018-04-18 19:34:02 +0200

    Extended parametrisation of test runner

--- src/tests/lomse_the_test_runner.cpp
+++ src/tests/lomse_the_test_runner.cpp
@@ -40,19 +40,85 @@ using namespace std;
 using namespace lomse;
 using namespace UnitTest;
 
+const char* basefilename(const char* fullfilename)
+{
+    const char* onlyFilenamePosix = strrchr(fullfilename, '/');
+    const char* onlyFilenameWin = strrchr(fullfilename, '\\');
+    const char* basename = std::max(onlyFilenamePosix, onlyFilenameWin) + 1;
+    return basename;
+}
+
+class MyTestReporterStdout : public UnitTest::TestReporterStdout
+{
+public:
+    MyTestReporterStdout(bool verbose) : m_verbose(verbose) {}
+
+    void ReportTestStart(TestDetails const& test) override
+    {
+        if (m_verbose)
+        {
+            printf("%s / %s / %s\n", basefilename(test.filename), test.suiteName, test.testName);
+        }
+    }
+
+    void ReportFailure(TestDetails const& test, char const* failure) override
+    {
+        if (m_verbose)
+        {
+            printf("    Failure at line %d: %s\n", test.lineNumber, failure);
+        }
+        else
+        {
+            printf("%s(%d): Failure in %s / %s: %s\n",
+                basefilename(test.filename), test.lineNumber, test.suiteName, test.testName, failure);
+        }
+    }
+
+    void ReportSummary(int totalTestCount, int failedTestCount,
+        int failureCount, float secondsElapsed) override
+    {
+        if (failureCount > 0)
+            printf("\nFailure: %d out of %d tests failed (%d failures).\n", failedTestCount, totalTestCount, failureCount);
+        else
+            printf("\nSuccess: %d tests passed.\n", totalTestCount);
+
+        printf("Test time: %.2f seconds.\n", secondsElapsed);
+    }
+
+private:
+    bool m_verbose;
+    string m_lastFailed;
+};
+
+bool TestMatches(TestDetails const& test, const string& name)
+{
+    return name == test.suiteName || name == test.testName ||
+        name == basefilename(test.filename);
+}
+
 int main(int argc, char** argv)
 {
-    //Based on code published here:
-    //  http://stackoverflow.com/questions/3546054/how-do-i-run-a-single-test-with-unittest
-    //
     //invoke without arguments to run all tests:
     //  testlib
     //
     //invoke with arguments to run a single test:
     //  testlib MyTestName
     //
-    //or single suite
-    //  testlib suite MySuite
+    //or single suite:
+    //  testlib MySuite
+    //
+    //or single filename:
+    //  testlib filename.cpp
+    //
+    //exclude Test/Suite/filename:
+    //  testlib -MyTestNameOrSuiteNameOrFilename
+    //
+    //verbose output:
+    //  testlib -v
+    //  testlib --verbose
+    //  testlib -v MyTestName
+    //  testlib -v MySuite
+    //  testlib -v filename.cpp
 
     cout << "Lomse version " << LibraryScope::get_version_long_string()
          << ". Library tests runner." << endl << endl;
@@ -60,58 +126,45 @@ int main(int argc, char** argv)
     cout << "Lomse build date: " << LibraryScope::get_build_date() << endl;
     cout << "Path for tests scores: '" << TESTLIB_SCORES_PATH << "'" << endl << endl;
 
+    bool verbose = argc > 1 && (!strcmp(argv[1], "--verbose") || !strcmp(argv[1], "-v"));
+    int nextArg = verbose ? 2 : 1;
+
+    MyTestReporterStdout reporter(verbose);
+    UnitTest::TestRunner runner(reporter);
+
     int nErrors = 0;
 
-    if( argc > 1 )
+    if (argc > nextArg)
     {
-        //if first arg is "suite", we search for suite names instead of test names
-        const bool fSuite = strcmp( "suite", argv[1] ) == 0;
+        cout << "Running some tests " << endl << endl;
 
-        if (fSuite)
-            cout << "Running tests in suite " << argv[2] << endl << endl;
-        else
-            cout << "Running some tests " << endl << endl;
-
-        //walk list of all tests, add those with a name that
-        //matches one of the arguments  to a new TestList
-        const TestList& allTests( Test::GetTestList() );
-        TestList selectedTests;
-        Test* p = allTests.GetHead();
-        while( p )
-        {
-            for( int i = 1 ; i < argc ; ++i )
+        //run only matching tests
+        nErrors = runner.RunTestsIf(Test::GetTestList(), nullptr,
+            [nextArg, argc, argv](Test* p)
             {
-                if( strcmp( fSuite ? p->m_details.suiteName
-                                   : p->m_details.testName, argv[ i ] ) == 0 )
-                    selectedTests.Add( p );
+                bool hasPositiveArgs = false;
+                bool positiveMatch = false;
+                bool negativeMatch = false;
+                for (int i = nextArg ; i < argc ; ++i)
+                {
+                    const char* arg = argv[i];
+                    bool negative = arg[0] == '-';
+                    negativeMatch |= negative && TestMatches(p->m_details, arg + 1);
+                    positiveMatch |= !negative && TestMatches(p->m_details, arg);
+                    hasPositiveArgs |= !negative;
+                }
+                return (!hasPositiveArgs || positiveMatch) && !negativeMatch;
             }
-            Test* q = p;
-
-#if (LOMSE_UNITTEST_OLD == 1)
-            p = p->next;
-            q->next = nullptr;
-#else
-            p = p->m_nextTest;
-            q->m_nextTest = nullptr;
-#endif
-        }
-
-        //run selected test(s) only
-        UnitTest::TestReporterStdout reporter;
-        UnitTest::TestRunner runner( reporter );
-        nErrors = runner.RunTestsIf( selectedTests, 0, True(), 0 );
+            , 0);
     }
     else
     {
         cout << "Running all tests" << endl << endl;
-        nErrors = UnitTest::RunAllTests();
+        nErrors = runner.RunTestsIf(Test::GetTestList(), nullptr, True(), 0);
     }
 
     #if defined WIN32 || defined _WIN32
-        //system("pause");
-        cin.get();
         _CrtDumpMemoryLeaks();
-
     #endif
 
     return nErrors;
