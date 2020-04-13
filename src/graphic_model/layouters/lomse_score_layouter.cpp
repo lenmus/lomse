be883dfd441afceb0001f2bdced5334a581da933
cecilios <s.cecilio@gmail.com>
2020-04-13 11:03:44 +0200

    When not enough space for one system overrun paper instead of err.msg.

--- src/graphic_model/layouters/lomse_score_layouter.cpp
+++ src/graphic_model/layouters/lomse_score_layouter.cpp
@@ -191,16 +191,23 @@ void ScoreLayouter::layout_in_box()
             //TODO: ScoreLayouter::layout_in_box()
             //  If paper height is smaller than system height it is impossible to fit
             //  one system in a page. We have to split system horizontally (some staves in
-            //  one page and the others in next page). But this is not yet implemented.
-            //  Therefore, for now, just an error message.
-            add_error_message("ERROR: Not enough space for drawing just one system!");
-            stringstream msg;
-            msg << "  Page size too small for " << m_pScore->get_num_instruments()
-                << " instruments.";
-            add_error_message(msg.str());
-            set_layout_is_finished(true);
-            delete_system();
-            return;
+            //  one page and the others in next page) or to adjust scale.
+            //  For now, just an error message or overrun paper.
+            #if (0)
+                //Display error message
+                add_error_message("ERROR: Not enough space for drawing just one system!");
+                stringstream msg;
+                msg << "  Page size too small for " << m_pScore->get_num_instruments()
+                    << " instruments.";
+                add_error_message(msg.str());
+                set_layout_is_finished(true);
+                delete_system();
+                return;
+            #else
+                //Overrun paper
+                add_system_to_page();
+                fSystemsAdded = true;
+            #endif
         }
         else
         {
