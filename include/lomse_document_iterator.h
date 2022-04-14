6551e2451aa091206dbe523179eb34aa713e54eb
Cecilio Salmeron <s.cecilio@gmail.com>
2022-04-14 16:51:00 +0200

    Clean up DocIterator and remove commented out code

--- include/lomse_document_iterator.h
+++ include/lomse_document_iterator.h
@@ -12,143 +12,41 @@
 
 #include <stack>
 
-using namespace std;
-
 namespace lomse
 {
 
 //forward declarations
 class ImoDocument;
-class ImoScore;
 class ImoObj;
 class Document;
 
-
-////---------------------------------------------------------------------------------------
-//// interfaces for traversing specific elements
-////---------------------------------------------------------------------------------------
-//
-//class InterfaceScoreElmIterator
-//{
-//public:
-//    //positioning
-//    virtual void start_of_instrument(int instr)=0;    //to first staff obj in instr 0..n
-//    virtual void find_instrument(int instr)=0;        //instr = 0..n-1
-//};
-//
-
-
 //---------------------------------------------------------------------------------------
-// ElementIterator: base class for any document iterator. It provides the basic
-// capabilities for traversing elements, locating specific sub-elements, etc.
+// DocIterator: An iterator to traverse the first level children of a document, that
+// is, to traverse the content items of ImDocument without traversing each child content.
 //---------------------------------------------------------------------------------------
-
-class ElementIterator
-{
-protected:
-    ImoObj* m_pObj;      //element being traversed
-    ImoObj* m_pCurItem;  //current pointed child
-
-public:
-    ElementIterator(ImoObj* pObj);
-    virtual ~ElementIterator();
-
-    inline ImoObj* operator *() { return m_pCurItem; }
-
-    //positioning
-    inline void operator ++() { next(); }
-    inline void operator --() { prev(); }
-//    void point_to(int objType);
-    void point_to(ImoObj* pObj);
-    //void enter_element();
-//    void exit_element();
-//    void exit_all_to(ImoObj* pImo);     //exit elements until the received one
-//    void to_begin();
-//    //void start_of(long objType, int num);   //to first sub-element in element #num [0..n-1] of type 'objType'
-//
-//    //information
-//    bool is_pointing_to(long objType);
-//    inline bool is_out_of_range() { return *m_it == nullptr; }
-//
-protected:
-    virtual void next()=0;    //to next sibling element
-    virtual void prev()=0;    //to prev sibling element
-//    void clear_stack();
-
-};
-
-
-
-
-
-////---------------------------------------------------------------------------------------
-//// ScoreElmIterator: adds score traversing capabilities to the received cursor.
-//// The cursor must be pointing to the score when invoking the constructor
-////---------------------------------------------------------------------------------------
-//
-//class ScoreElmIterator //: public InterfaceScoreElmIterator
-//{
-//protected:
-//    ElementIterator* m_pParent;
-//    ImoScore* m_pScore;
-//
-//public:
-//    ScoreElmIterator(ElementIterator* pParent, ImoScore* pScore);
-//    virtual ~ScoreElmIterator();
-//
-////    //positioning
-////    void start_of_instrument(int instr);    //to first staff obj in instr 0..n
-////    void find_instrument(int instr);        //instr = 0..n-1
-//
-//protected:
-//    void start();   //to first element in score
-//
-//};
-
-
-
-//---------------------------------------------------------------------------------------
-// DocIterator: A cursor to traverse the document for interactive edition
-// - It uses the facade pattern to hide the particularities of traversing each
-//   document element type
-// - For traversing each element type it delegates on a specific cursor (adaptor
-//   pattern)
-//---------------------------------------------------------------------------------------
-
-class DocIterator : public ElementIterator      //, public InterfaceScoreElmIterator
+class DocIterator
 {
 protected:
     ImoDocument* m_pDoc;
+    ImoObj* m_pCurItem;  //current pointed child
     int m_numContentItems;
     int m_curItemIndex;
 
 public:
     DocIterator(Document* pDoc);
-    ~DocIterator() override;
 
     //positioning
     void start_of_content();
     void last_of_content();
+    inline void operator ++() { next(); }
+    inline void operator --() { prev(); }
 
-    ////overrides
-    //void enter_element();
-
-//    //implement InterfaceScoreElmIterator by delegation
-//    void start_of_instrument(int instr) {   //to first staff obj in instr 0..n
-//            if (m_pScoreElmIterator)
-//                m_pScoreElmIterator->start_of_instrument(instr);
-//        }
-//
-//    void find_instrument(int instr) {   //instr = 0..n-1
-//            if (m_pScoreElmIterator)
-//                m_pScoreElmIterator->find_instrument(instr);
-//        }
+    //access to content
+    inline ImoObj* operator *() { return m_pCurItem; }
 
 protected:
-    //ScoreElmIterator* m_pScoreElmIterator;
-
-    void next() override;
-    void prev() override;
+    void next();
+    void prev();
     void point_to_current();
 
 };
--- src/document/lomse_document_iterator.cpp
+++ src/document/lomse_document_iterator.cpp
@@ -12,127 +12,19 @@
 #include "private/lomse_document_p.h"
 #include "lomse_internal_model.h"
 
-using namespace std;
 
 namespace lomse
 {
 
 //---------------------------------------------------------------------------------------
-// ElementIterator implementation
-//---------------------------------------------------------------------------------------
-ElementIterator::ElementIterator(ImoObj* pObj)
-    : m_pObj(pObj)
-{
-    m_pCurItem = nullptr;    //m_pTree->begin();
-}
-
-//---------------------------------------------------------------------------------------
-ElementIterator::~ElementIterator()
-{
-}
-
-////---------------------------------------------------------------------------------------
-//void ElementIterator::to_begin()
-//{
-//    clear_stack();
-//    m_pCurItem = m_pTree->begin();
-//}
-//
-////---------------------------------------------------------------------------------------
-//void ElementIterator::clear_stack()
-//{
-//    while (!m_stack.empty())
-//        m_stack.pop();
-//}
-//
-////---------------------------------------------------------------------------------------
-//bool ElementIterator::is_pointing_to(long objType)
-//{
-//    return *m_pCurItem != nullptr && (*m_pCurItem)->get_type() == objType;
-//}
-//
-////---------------------------------------------------------------------------------------
-//void ElementIterator::point_to(int objType)
-//{
-//    while (*m_pCurItem != nullptr && !is_pointing_to(objType))
-//        next();
-//}
-//
-//---------------------------------------------------------------------------------------
-void ElementIterator::point_to(ImoObj* pObj)
-{
-    m_pCurItem = pObj;
-}
-
-////---------------------------------------------------------------------------------------
-//void ElementIterator::enter_element()
-//{
-//    if (*m_pCurItem != nullptr)
-//    {
-//        m_stack.push(m_pCurItem);
-//        ++m_pCurItem;
-//    }
-//}
-//
-////---------------------------------------------------------------------------------------
-//void ElementIterator::exit_element()
-//{
-//    if (!m_stack.empty())
-//    {
-//        m_pCurItem = m_stack.top();
-//        m_stack.pop();
-//    }
-//    else
-//        m_pCurItem = nullptr;
-//}
-//
-////---------------------------------------------------------------------------------------
-//void ElementIterator::exit_all_to(ImoObj* pImo)
-//
-//{
-//     //exit elements until the received one
-//
-//    while (*m_pCurItem != pImo && !m_stack.empty())
-//    {
-//        m_pCurItem = m_stack.top();
-//        if (*m_pCurItem == pImo)
-//            break;
-//        m_stack.pop();
-//    }
-//}
-//
-////---------------------------------------------------------------------------------------
-////void ElementIterator::start_of(long objType, int num)
-////{
-////    //within the limits of current element finds the element #num [0..n-1]
-////    //of type 'objType' and points to its first sub-element
-////
-////    to_begin();
-////    enter_element();
-////    point_to(k_content);
-////    enter_element();
-////}
-
-
-
-//---------------------------------------------------------------------------------------
 // DocIterator implementation
 //---------------------------------------------------------------------------------------
 DocIterator::DocIterator(Document* pDoc)
-    : ElementIterator(pDoc->get_im_root())
-    , m_pDoc(pDoc->get_im_root())
-    //, m_pScoreElmIterator(nullptr)
+    : m_pDoc(pDoc->get_im_root())
 {
     m_numContentItems = m_pDoc->get_num_content_items();
     m_curItemIndex = -1;
     next();
-}
-
-//---------------------------------------------------------------------------------------
-DocIterator::~DocIterator()
-{
-//    if (m_pScoreElmIterator)
-//        delete m_pScoreElmIterator;
 }
 
 //---------------------------------------------------------------------------------------
@@ -158,29 +50,9 @@ void DocIterator::point_to_current()
         m_pCurItem = nullptr;
 }
 
-////---------------------------------------------------------------------------------------
-//void DocIterator::enter_element()
-//{
-//    //Factory method
-//    //if new elements added, create specific cursor to delegate to it.
-//
-//    if (m_pCurItem->is_score())
-//    {
-//        if (m_pScoreElmIterator)
-//            delete m_pScoreElmIterator;
-//        m_pScoreElmIterator = LOMSE_NEW ScoreElmIterator(this, dynamic_cast<ImScore*>(m_pCurItem) );
-//    }
-//}
-
 //---------------------------------------------------------------------------------------
 void DocIterator::start_of_content()
 {
-    //to first item in 'content' element
-
-//    to_begin();
-//    enter_element();
-//    point_to(k_content);
-//    enter_element();
     m_curItemIndex = -1;
     next();
 }
@@ -188,62 +60,10 @@ void DocIterator::start_of_content()
 //---------------------------------------------------------------------------------------
 void DocIterator::last_of_content()
 {
-//    to_begin();
-//    enter_element();
-//    point_to(k_content);
-//    m_pCurItem = (*m_pCurItem)->get_last_child();
     m_numContentItems = m_pDoc->get_num_content_items();
     m_curItemIndex = m_numContentItems - 2;
     next();
 }
-
-
-
-////---------------------------------------------------------------------------------------
-//// ScoreElmIterator implementation
-////---------------------------------------------------------------------------------------
-//ScoreElmIterator::ScoreElmIterator(ElementIterator* pParent, ImScore* pScore)
-//    : m_pParent(pParent)
-//    , m_pScore(pScore)
-//{
-//}
-//
-////---------------------------------------------------------------------------------------
-//ScoreElmIterator::~ScoreElmIterator()
-//{
-//}
-
-////---------------------------------------------------------------------------------------
-//void ScoreElmIterator::start()
-//{
-//    m_pParent->exit_all_to(m_pScore);
-//    m_pParent->enter_element();
-//}
-//
-////---------------------------------------------------------------------------------------
-//void ScoreElmIterator::start_of_instrument(int instr)
-//{
-//    //to first staff obj of instr (0..n-1)
-//
-//    find_instrument(instr);
-//    m_pParent->enter_element();
-//    m_pParent->point_to(k_musicData);
-//    m_pParent->enter_element();
-//}
-//
-////---------------------------------------------------------------------------------------
-//void ScoreElmIterator::find_instrument(int instr)
-//{
-//    //instr = 0..n
-//
-//    start();
-//    m_pParent->point_to(k_instrument);
-//    for (int i=0; i != instr && !m_pParent->is_out_of_range(); i++)
-//    {
-//        ++(*m_pParent);
-//        m_pParent->point_to(k_instrument);
-//    }
-//}
 
 
 }  //namespace lomse
